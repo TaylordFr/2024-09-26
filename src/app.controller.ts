8fd72fb4c3cab44d8ecf021f21ea0616d90aa8d6
Petrik Diak <diak@petrik.hu>
2024-09-26 15:14:47 +0200

    hatterszin, jegkremek, jegkremlista

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Query, Param} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -11,5 +11,38 @@ export class AppController {
     return {
       message: this.appService.getHello()
     };
+  }
+
+  @Get('hatterszin')
+  @Render('hatter')
+  hatterszin(@Query('bgColor') bgColor: string = 'blue'){
+      return {
+        bgColor
+      }
+  }
+
+
+  #jegkremek =  [
+    { nev:'Eperfagylalt', ar:450},
+    { nev:'Vanília', ar:500},
+    { nev:'Csokoládé', ar:550 }
+  ]
+
+  @Get('jegkrem')
+  @Render('jegkremlista')
+  jegkremLista(){
+    return{
+      jegkremek: this.#jegkremek
+    }
+  }
+
+
+
+  @Get('jegkrem/:id')
+  @Render('jegkrem')
+  jegkrem(@Param('id') id: string){
+
+    return this.#jegkremek[id]
+
   }
 }
